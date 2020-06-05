a74a19a6ab09a10db19db7d8ae6124b5b901f493
BERNARDO ESPINOZA <b-j-espinoza@hotmail.com>
2020-06-04 21:34:54 -0300

    Logo e icono - Pantilla Index.

---
+++ solucion.ecommerce/proyecto.ecommerce/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace proyecto.ecommerce.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Productos()
+        {
+            return View();
+        }
+        public ActionResult Categorias()
+        {
+            return View();
+        }
+        public ActionResult Cuenta()
+        {
+            return View();
+        }
+        public ActionResult Contacto()
+        {
+            return View();
+        }
+        public ActionResult Compra()
+        {
+            return View();
+        }
+    }
+}
